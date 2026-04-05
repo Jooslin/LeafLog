6a5a85c1d98bbfee1d272246171bc7f0925f031b
t2025-m0143 <>
2026-04-05 21:52:05 +0900

    ✨feat: AppFlow 생성 및 window 설정

---
+++ LeafLog/LeafLog/App/AppStep.swift
@@ -0,0 +1,13 @@
+//
+//  AppStep.swift
+//  LeafLog
+//
+//  Created by t2025-m0143 on 4/5/26.
+//
+
+import RxFlow
+
+enum AppStep: Step {
+    case root
+    
+}
--- LeafLog/LeafLog/SceneDelegate.swift
+++ LeafLog/LeafLog/SceneDelegate.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import RxFlow
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    let coordinator = FlowCoordinator()
+    
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -16,10 +19,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
+//        window = UIWindow(windowScene: windowScene)
+//        
+//        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+//        window?.makeKeyAndVisible()
         
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
-        window?.makeKeyAndVisible()
+        let appFlow = AppFlow(windowScene: windowScene)
+        window = appFlow.window
+        
+        coordinator.coordinate(flow: appFlow, with: OneStepper(withSingleStep: AppStep.root))
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
