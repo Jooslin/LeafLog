740f556a3e8eebeb5b00394f629d5a9605e4c6ef
t2025-m0143 <>
2026-04-06 21:45:50 +0900

    ✨feat: 모델 열거형 생성

--- LeafLog/LeafLog/Service/PlantClassificationService/PlantClassificationService.swift
+++ LeafLog/LeafLog/Service/PlantClassificationService/PlantClassificationService.swift
@@ -9,14 +9,29 @@ import TensorFlowLite
 import UIKit
 
 class PlantClassificationService {
+    enum Model {
+        case aiyPlantsV1
+        
+        var modelPath: String? {
+            switch self {
+            case .aiyPlantsV1:
+                return Bundle.main.path(forResource: "3", ofType: "tflite")
+            }
+        }
+    }
+    
+    let model = Model.aiyPlantsV1
+    
     // 이미지 크기
     private let inputWidth = 224
     private let inputHeight = 224
     
     func analyzeImage(image: UIImage) {
-        guard let mediaPath = Bundle.main.path(forResource: "3", ofType: "tflite") else {
+        guard let mediaPath = model.modelPath else {
             print("모델 파일을 찾을 수 없습니다.")
             return
         }
+        
+        
     }
 }
