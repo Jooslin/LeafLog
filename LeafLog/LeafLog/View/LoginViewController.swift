592c79b4589c420d4816e332eb306ae6df982827
coduhee <vdyjb@ewha.ac.kr>
2026-04-07 12:44:43 +0900

    ✨feat: LoginViewController 구현

---
+++ LeafLog/LeafLog/View/LoginViewController.swift
@@ -0,0 +1,75 @@
+//
+//  LoginViewController.swift
+//  LeafLog
+//
+//  Created by 김주희 on 4/3/26.
+//
+
+import UIKit
+import ReactorKit
+import RxSwift
+import RxCocoa
+
+class LoginViewController: UIViewController, View {
+    
+    var disposeBag = DisposeBag()
+    
+    private let loginView = LoginView()
+    
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        view = loginView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+    // MARK: - Bind
+    func bind(reactor: LoginReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    
+    private func bindAction(reactor: LoginReactor) {
+        loginView.googleLoginButton.rx.tap
+            .map { LoginReactor.Action.googleLoginTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        loginView.kakaoLoginButton.rx.tap
+            .map { LoginReactor.Action.kakaoLoginTapped }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+    }
+    
+    
+    private func bindState(reactor: LoginReactor) {
+        let state = reactor.state.asDriver(onErrorJustReturn: LoginReactor.State())
+        
+        reactor.pulse(\.$loginSuccess)
+            .filter { $0 == true }
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] _ in
+                self?.transitionToMain()
+            })
+            .disposed(by: disposeBag)
+
+        
+        reactor.pulse(\.$errorMessage)
+            .compactMap { $0 }
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] message in
+                self?.showAlert(message: message)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func transitionToMain() {
+        // TODO: 실제 메인 뷰 컨트롤러로 넘어가는 코드 작성
+    }
+}
