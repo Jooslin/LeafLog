c7828515be41901db808775a32633e3ba7f21ef2
t2025-m0143 <>
2026-04-07 22:05:37 +0900

    🩹chore: 개인 테스트 코드 삭제

--- LeafLog/LeafLog/View/ViewController.swift
+++ LeafLog/LeafLog/View/ViewController.swift
@@ -10,50 +10,9 @@ import SnapKit
 
 class ViewController: UIViewController {
 
-    let plantService = PlantClassificationService()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        view.backgroundColor = .white
-        let imageView = UIImageView(image: .monstera)
-        let button = UIButton(configuration: .plain())
-        button.setTitle("analyze", for: .normal)
-        let resultLabel = UILabel()
-        
-        view.addSubview(imageView)
-        view.addSubview(button)
-        view.addSubview(resultLabel)
-        
-        imageView.snp.makeConstraints {
-            $0.width.height.equalTo(300)
-            $0.center.equalToSuperview()
-        }
-        
-        button.snp.makeConstraints {
-            $0.top.equalTo(imageView.snp.bottom).offset(10)
-            $0.centerX.equalToSuperview()
-        }
-        
-        resultLabel.snp.makeConstraints {
-            $0.top.equalTo(button.snp.bottom).offset(10)
-            $0.centerX.equalToSuperview()
-        }
-        
-        button.addAction(UIAction { _ in
-            guard let image = imageView.image else {
-                print("이미지가 없음")
-                return
-            }
-            
-            do {
-                let result = try self.plantService.analyzeImage(image: image)
-                resultLabel.text = "정확도 \(result.0), 식물 이름: \(result.1)"
-            } catch {
-                resultLabel.text = "분석 실패, 원인:\(error.localizedDescription)"
-            }
-        }, for: .touchUpInside)
     }
 
 
