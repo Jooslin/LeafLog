1a39cdc4a6d1932dfd119ba8fc07af536bd3727f
coduhee <vdyjb@ewha.ac.kr>
2026-04-07 10:07:42 +0900

    ✨feat: AppSecrets에 kakaoNativeAppKey, googleClientID 추가

--- LeafLog/LeafLog/Configuration/AppSecrets.swift
+++ LeafLog/LeafLog/Configuration/AppSecrets.swift
@@ -12,7 +12,10 @@ enum AppSecrets {
     private enum InfoKeys {
         static let url = "SUPABASE_URL"
         static let anonKey = "SUPABASE_ANON_KEY"
+        static let kakaoNativeAppKey = "KAKAO_NATIVE_APP_KEY"
+        static let googleClientID = "CLIENT_ID"
     }
+    
     
     static func getSecret(_ key: String) -> String {
         guard let value = Bundle.main.object(forInfoDictionaryKey: key) as? String else {
@@ -21,6 +24,26 @@ enum AppSecrets {
         return value
     }
 
+    
     static var supabaseURL: String { Self.getSecret(InfoKeys.url) }
     static var supabaseAnonKey: String { Self.getSecret(InfoKeys.anonKey) }
+    static var kakaoNativeAppKey: String {
+        let key = Self.getSecret(InfoKeys.kakaoNativeAppKey)
+        
+        if key.isEmpty || key.contains("$") {
+            assertionFailure("Info.plist가 가짜 \(key)값을 가지고 있습니다.")
+            return ""
+        }
+        return key
+    }
+    static var googleClientID: String {
+        guard let plistPath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
+              let plistDict = NSDictionary(contentsOfFile: plistPath) as? [String: Any],
+              let clientID = plistDict[InfoKeys.googleClientID] as? String,
+              !clientID.isEmpty else {
+            assertionFailure("GoogleService-Info.plist 파일을 찾을 수 없거나 'CLIENT_ID'값이 존재하지 않습니다.")
+            return ""
+        }
+        return clientID
+    }
 }
