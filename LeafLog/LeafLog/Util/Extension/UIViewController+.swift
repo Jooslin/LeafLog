9b642f147d131e70a15f5e683b64fa33b3cf3fee
devBambu <byrr113@gmail.com>
2026-04-08 10:19:25 +0900

    ✨feat: viewDidLoad 확장 삭제

--- LeafLog/LeafLog/Util/Extension/UIViewController+.swift
+++ LeafLog/LeafLog/Util/Extension/UIViewController+.swift
@@ -15,10 +15,6 @@ extension Reactive where Base: UIViewController {
         methodInvoked(#selector(Base.viewWillAppear(_:))).map { _ in }
     }
     
-    var viewDidLoad: Observable<Void> {
-        methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
-    }
-    
     var viewWillDisappear: Observable<Void> {
         methodInvoked(#selector(Base.viewWillDisappear(_:))).map { _ in }
     }
