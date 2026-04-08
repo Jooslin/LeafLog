eaf5e8145f35c19a82845b4054ddd7926e50bc23
t2025-m0143 <>
2026-04-08 09:33:38 +0900

    ✨feat: Reactive - UIViewController 확장

---
+++ LeafLog/LeafLog/Util/Extension/UIViewController+.swift
@@ -0,0 +1,25 @@
+//
+//  Extension.swift
+//  LeafLog
+//
+//  Created by t2025-m0143 on 4/3/26.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+// ViewController의 메서드를 Observable 객체로 사용할 수 있도록 확장
+extension Reactive where Base: UIViewController {
+    var viewWilAppear: Observable<Void> {
+        methodInvoked(#selector(Base.viewWillAppear(_:))).map { _ in }
+    }
+    
+    var viewDidLoad: Observable<Void> {
+        methodInvoked(#selector(Base.viewDidAppear(_:))).map { _ in }
+    }
+    
+    var viewWillDisappear: Observable<Void> {
+        methodInvoked(#selector(Base.viewWillDisappear(_:))).map { _ in }
+    }
+}
