fc21a4b6922971b9662bcaeabe175ff8c0d5c326
devBambu <byrr113@gmail.com>
2026-04-08 20:24:56 +0900

    ✨feat: 파이어베이스 FCM 기본 설정

--- LeafLog/LeafLog/AppDelegate.swift
+++ LeafLog/LeafLog/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import KakaoSDKCommon
 import GoogleSignIn
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // 카카오 SDK 세팅
         KakaoSDK.initSDK(appKey: AppSecrets.kakaoNativeAppKey)
+        
+        // 파이어베이스 설정
+        FirebaseApp.configure()
+        
+        // 앱 실행 시 사용자에게 알림 허용 권한 받기
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        // Apple의 푸시 서버(APNs)에 앱을 등록하고 디바이스 토큰 발급을 요청함
+        application.registerForRemoteNotifications()
+        
+        // 파이어베이스 Messaging 설정
+        Messaging.messaging().delegate = self
         
         return true
         }
@@ -40,3 +59,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 }
 
+extension AppDelegate {
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // Foreground에서 알림이 오는 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner])
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    // 파이어베이스 MessagingDelegate 설정 - 앱 시작 시, 혹은 토큰이 갱신되었을 때 호출되는 함수
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+    }
+}
