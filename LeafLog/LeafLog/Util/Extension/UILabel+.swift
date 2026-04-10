425be1355dd6eb1b04ea57e11084b07eac3244fb
yeseul jang <yyssjj9999@gmail.com>
2026-04-10 17:06:56 +0900

    🌟feat: LabelConfiguration 추가

---
+++ LeafLog/LeafLog/Util/Extension/UILabel+.swift
@@ -0,0 +1,25 @@
+//
+//  UILabel+.swift
+//  LeafLog
+//
+//  Created by Yeseul Jang on 4/10/26.
+//
+import UIKit
+
+extension UILabel {
+    func apply(
+        _ config: LabelConfiguration,
+        color: UIColor? = nil,
+        lines: Int? = nil
+    ) {
+        self.font = config.font
+        self.textColor = color ?? config.color
+        self.numberOfLines = lines ?? config.lines
+    }
+    
+    convenience init(text: String = "", config: LabelConfiguration) {
+        self.init()
+        self.text = text
+        apply(config)
+    }
+}
