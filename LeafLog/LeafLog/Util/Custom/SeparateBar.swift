6a0728ea5e765793254516fa1e733f6c76bbfb2d
devBambu <byrr113@gmail.com>
2026-04-12 22:09:30 +0900

    ♻️refactor: 높이 설정 코드 SnapKit -> intrinsicContentSize로 변경

--- LeafLog/LeafLog/Util/Custom/SeparateBar.swift
+++ LeafLog/LeafLog/Util/Custom/SeparateBar.swift
@@ -6,20 +6,19 @@
 //
 
 import UIKit
-import SnapKit
 
 class SeparateBar: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = UIColor(red: 0.89, green: 0.89, blue: 0.89, alpha: 1.00) // HEX #E3E3E3
-        
-        self.snp.makeConstraints {
-            $0.height.equalTo(1)
-        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+            return CGSize(width: UIView.noIntrinsicMetric, height: 1)
     }
 }
