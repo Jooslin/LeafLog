5fa8562e67cfb2b1be86394dd60ecc6f06d21d16
devBambu <byrr113@gmail.com>
2026-04-12 20:56:48 +0900

    ✨feat: BaseCardView 구현

--- LeafLog/LeafLog/Util/Custom/BaseCardView.swift
+++ LeafLog/LeafLog/Util/Custom/BaseCardView.swift
@@ -1,5 +1,5 @@
 //
-//  SeparateBar.swift
+//  BaseCardView.swift
 //  LeafLog
 //
 //  Created by t2025-m0143 on 4/12/26.
@@ -8,15 +8,14 @@
 import UIKit
 import SnapKit
 
-class SeparateBar: UIView {
+class BaseCardView: UIView {
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = UIColor(red: 0.89, green: 0.89, blue: 0.89, alpha: 1.00) // HEX #E3E3E3
-        
-        self.snp.makeConstraints {
-            $0.height.equalTo(1)
-        }
+        backgroundColor = UIColor(red: 0.97, green: 0.97, blue: 0.97, alpha: 1.00) // HEX #F7F7F7
+        layer.cornerRadius = 8
+        clipsToBounds = true
     }
     
     required init?(coder: NSCoder) {
