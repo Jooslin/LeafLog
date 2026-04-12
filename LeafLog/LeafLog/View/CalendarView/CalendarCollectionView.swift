08d3d3f993b55147ca69a11fe9866f1775cb430b
devBambu <byrr113@gmail.com>
2026-04-12 23:22:25 +0900

    ✨feat: CalendarCollectionView 생성, 달력 표시부 compositional layout 구성

--- LeafLog/LeafLog/View/CalendarView/CalendarCollectionView.swift
+++ LeafLog/LeafLog/View/CalendarView/CalendarCollectionView.swift
@@ -4,3 +4,43 @@
 //
 //  Created by t2025-m0143 on 4/12/26.
 //
+
+import UIKit
+
+final class CalendarCollectionView: UICollectionView {
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: UICollectionViewLayout())
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CalendarCollectionView {
+    private func makeCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        
+        return UICollectionViewCompositionalLayout(sectionProvider: { sectionIndex, environment in
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .absolute(42),
+                    heightDimension: .absolute(74)
+                ))
+            
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .absolute(74)
+                ),
+                repeatingSubitem: item,
+                count: 7
+            )
+            
+            let section = NSCollectionLayoutSection(group: group)
+            
+            return section
+        }, configuration: configuration)
+    }
+}
