45aacb829810eea76e2dbd3f5320d6fb51b55b79
yeseul jang <yyssjj9999@gmail.com>
2026-04-13 17:26:29 +0900

    🌟feat: CornerRadius14TextButton 구현

---
+++ LeafLog/LeafLog/Util/Custom/CornerRadius14Button.swift
@@ -0,0 +1,48 @@
+//
+//  CornerRadius14Button.swift
+//  LeafLog
+//
+//  Created by Yeseul Jang on 4/13/26.
+//
+
+import UIKit
+
+/*
+ 생성 시
+ let button = CornerRadius14Button(title: "선택")
+ 
+ 이름 변경시
+ button.apply(title: "완료")
+ */
+
+class CornerRadius14Button: UIButton {
+    init(title: String) {
+        super.init(frame: .zero)
+        apply(title: title)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func apply(title: String) {
+        var configuration = UIButton.Configuration.plain()
+        configuration.title = title
+        // TODO: 색 확정시 수정 요망
+        configuration.baseForegroundColor = UIColor.darkGray
+        configuration.background.backgroundColor = UIColor(red: 0.89, green: 0.89, blue: 0.89, alpha: 1)
+        configuration.background.cornerRadius = 12
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12)
+        
+        // 다이나믹 폰트 설정
+        configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            let baseFont = UIFont.systemFont(ofSize: 14, weight: .medium)
+            outgoing.font = UIFontMetrics(forTextStyle: .subheadline).scaledFont(for: baseFont)
+            return outgoing
+        }
+        self.configuration = configuration
+        titleLabel?.adjustsFontForContentSizeCategory = true
+    }
+}
+
