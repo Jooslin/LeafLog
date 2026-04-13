b438d499a2f731c035d2f82c2568008c7c651653
devBambu <byrr113@gmail.com>
2026-04-13 19:59:47 +0900

    ✨feat: CalendarDetailCell 생성

---
+++ LeafLog/LeafLog/View/CalendarView/CalendarDetailCell.swift
@@ -0,0 +1,62 @@
+//
+//  CalendarDetailCell.swift
+//  LeafLog
+//
+//  Created by t2025-m0143 on 4/13/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class CalendarDetailCell: UICollectionViewCell {
+    
+    //TODO: LabelConfiguration 적용 시 주석 해제
+//    private let nameLabel = UILabel(text: "몬스테라", config: .body14)
+    private let nameLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .medium)
+        $0.text = "0"
+    }
+    
+    private let colorChip = UIView().then {
+        $0.layer.cornerRadius = 4
+        $0.clipsToBounds = true
+        $0.snp.makeConstraints {
+            $0.width.height.equalTo(8)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CalendarDetailCell {
+    private func setLayout() {
+        let stackView = generateStackView()
+        contentView.addSubview(stackView)
+        
+        stackView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.verticalEdges.equalToSuperview().inset(12)
+        }
+    }
+    
+    private func generateStackView() -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: [colorChip, nameLabel]).then {
+            $0.axis = .horizontal
+            $0.spacing = 8
+            $0.alignment = .center
+        }
+        
+        colorChip.setContentHuggingPriority(.required, for: .horizontal)
+        colorChip.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
+        return stackView
+    }
+}
