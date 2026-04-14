add53d2edd38463795ba74031b06a457cc3f674c
devBambu <byrr113@gmail.com>
2026-04-14 20:01:51 +0900

    ✨feat: HomeView 생성

---
+++ LeafLog/LeafLog/View/HomeView/HomeView.swift
@@ -0,0 +1,14 @@
+//
+//  HomeView.swift
+//  LeafLog
+//
+//  Created by t2025-m0143 on 4/14/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class HomeView: UIView {
+    let titleView = TitleHeaderView(text: "", hasBackButton: false, rightButtonImage: "bell")
+}
