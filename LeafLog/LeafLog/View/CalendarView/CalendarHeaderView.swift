03665d294b65655d4b49d21b0b613d36f29707eb
devBambu <byrr113@gmail.com>
2026-04-14 09:57:21 +0900

    ✨feat: calendar headerview 구현

---
+++ LeafLog/LeafLog/View/CalendarView/CalendarHeaderView.swift
@@ -0,0 +1,58 @@
+//
+//  CalendarHeaderView.swift
+//  LeafLog
+//
+//  Created by t2025-m0143 on 4/14/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class CalendarHeaderView: UIView {
+    let previousButton = UIButton(configuration: .plain())
+    let nextButton = UIButton(configuration: .plain())
+    let dateLabel = UILabel(config: .label16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setButtonAttributes()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CalendarHeaderView {
+    private func setButtonAttributes() {
+        previousButton.setImage(.arrowLeft, for: .normal)
+        nextButton.setImage(.arrowRight, for: .normal)
+    }
+    
+    private func setLayout() {
+        let stackView = generateStackView()
+        
+        addSubview(stackView)
+        
+        stackView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview().inset(12)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    private func generateStackView() -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: [previousButton, dateLabel, nextButton]).then {
+            $0.axis = .horizontal
+            $0.spacing = 32
+        }
+        
+        previousButton.setContentHuggingPriority(.required, for: .horizontal)
+        previousButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+        nextButton.setContentHuggingPriority(.required, for: .horizontal)
+        nextButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        return stackView
+    }
+}
