60a837068ffc87f5fd5802e8ca82869a67f8a713
yeseul jang <yyssjj9999@gmail.com>
2026-04-15 21:31:19 +0900

    🚚build: 서치 인포 뷰 컨트롤러 추가 및 구현

---
+++ LeafLog/LeafLog/View/SearchView/SearchInfoView/SearchInfoViewController.swift
@@ -0,0 +1,66 @@
+//
+//  SearchInfoViewController.swift
+//  LeafLog
+//
+//  Created by Yeseul Jang on 4/15/26.
+//
+import SnapKit
+import Then
+import UIKit
+
+final class SearchInfoViewController: UIViewController {
+    private let infoView = SearchInfoView()
+    private let dimmedView = UIView().then {
+        $0.backgroundColor = UIColor.black.withAlphaComponent(0.20)
+    }
+    private let cardContainerView = UIView().then {
+        $0.backgroundColor = .systemBackground
+        $0.layer.cornerRadius = 12
+        $0.layer.masksToBounds = true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        modalPresentationStyle = .overFullScreen
+        view.backgroundColor = .clear
+        setupUI()
+        bindUI()
+    }
+
+    private func setupUI() {
+        view.addSubview(dimmedView)
+        view.addSubview(cardContainerView)
+        cardContainerView.addSubview(infoView)
+
+        dimmedView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        cardContainerView.snp.makeConstraints {
+            $0.centerY.equalToSuperview().priority(750)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.verticalEdges.greaterThanOrEqualTo(view.safeAreaLayoutGuide).inset(32)
+        }
+
+        infoView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    private func bindUI() {
+        dimmedView.addGestureRecognizer(
+            UITapGestureRecognizer(target: self, action: #selector(didTapDimmedView))
+        )
+
+        infoView.closeButton.addAction(
+            UIAction { [weak self] _ in
+                self?.dismiss(animated: false)
+            },
+            for: .touchUpInside
+        )
+    }
+
+    @objc private func didTapDimmedView() {
+        dismiss(animated: false)
+    }
+}
