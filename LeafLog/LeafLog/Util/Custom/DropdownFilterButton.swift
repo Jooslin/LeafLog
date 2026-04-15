d45c582f17c668bdb8d477b04dcabead68463e73
yeseul jang <yyssjj9999@gmail.com>
2026-04-15 10:05:53 +0900

    🌟feat:  DropdownFilterButton 색 변경 할 수 있도록 기능 추가

--- LeafLog/LeafLog/Util/Custom/DropdownFilterButton.swift
+++ LeafLog/LeafLog/Util/Custom/DropdownFilterButton.swift
@@ -13,19 +13,24 @@ import UIKit
 
  이름 변경시
  button.apply(title: "잎색")
+
+ 선택 상태 색상 변경 시
+ applySelectionStyle(isSelected:)
+ isSelected가 true일 경우 연두색으로 표시됩니다.
  */
 
 final class DropdownFilterButton: UIButton {
     init(title: String) {
         super.init(frame: .zero)
         apply(title: title)
+        applySelectionStyle(isSelected: false)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func apply(title: String, isSelected: Bool = false) {
+    func apply(title: String) {
         var configuration = UIButton.Configuration.plain()
         configuration.title = title
         configuration.image = UIImage(systemName: "chevron.down")
@@ -33,11 +38,11 @@ final class DropdownFilterButton: UIButton {
             UIImage.SymbolConfiguration(pointSize: 10, weight: .medium)
         configuration.imagePlacement = .trailing
         configuration.imagePadding = 4
-        configuration.baseForegroundColor = isSelected ? .primary700 : .grayScale500
-        configuration.background.backgroundColor = isSelected ? .primary200 : .white
+        configuration.baseForegroundColor = .grayScale500
+        configuration.background.backgroundColor = .white
         configuration.background.cornerRadius = 12
         configuration.background.strokeWidth = 1
-        configuration.background.strokeColor = isSelected ? .primary700 : .grayScale100
+        configuration.background.strokeColor = .grayScale100
         configuration.contentInsets = NSDirectionalEdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12)
 
         configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
@@ -46,6 +51,16 @@ final class DropdownFilterButton: UIButton {
             outgoing.font = UIFontMetrics(forTextStyle: .body).scaledFont(for: baseFont)
             return outgoing
         }
+
+        self.configuration = configuration
+    }
+
+    func applySelectionStyle(isSelected: Bool) {
+        guard var configuration = self.configuration else { return }
+
+        configuration.baseForegroundColor = isSelected ? .primary700 : .grayScale500
+        configuration.background.backgroundColor = isSelected ? .primary200 : .white
+        configuration.background.strokeColor = isSelected ? .primary700 : .grayScale100
 
         self.configuration = configuration
     }
