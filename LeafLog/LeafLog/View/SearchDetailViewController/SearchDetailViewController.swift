5873a43cfce371d8a8a140023837d0e529fc0efb
yeseul jang <yyssjj9999@gmail.com>
2026-04-16 12:40:36 +0900

    🌟feat: required init 추가

--- LeafLog/LeafLog/View/SearchDetailViewController/SearchDetailViewController.swift
+++ LeafLog/LeafLog/View/SearchDetailViewController/SearchDetailViewController.swift
@@ -20,7 +20,11 @@ final class SearchDetailViewController: UIViewController, View {
         super.init(nibName: nil, bundle: nil)
         self.reactor = reactor
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
