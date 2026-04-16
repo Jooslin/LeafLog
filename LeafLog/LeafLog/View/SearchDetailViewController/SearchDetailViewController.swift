835f6971dcaf780f1d8c3b398237f59103afc556
yeseul jang <yyssjj9999@gmail.com>
2026-04-16 12:36:11 +0900

    🚚build: SearchDetailViewController 추가 및 데이터 확인을 위한 기본 구현

---
+++ LeafLog/LeafLog/View/SearchDetailViewController/SearchDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SearchDetailViewController.swift
+//  LeafLog
+//
+//  Created by Yeseul Jang on 4/16/26.
+//
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import UIKit
+import Then
+
+final class SearchDetailViewController: UIViewController, View {
+
+    var disposeBag = DisposeBag()
+
+    init(reactor: SearchDetailReactor) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+    func bind(reactor: SearchDetailReactor) {
+
+        // 화면 뜨면서
+        Observable.just(())
+            .map { SearchDetailReactor.Action.viewDidLoad }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        // 데이터 들어오는지 확인
+        reactor.state
+            .compactMap { $0.detail }
+            .subscribe(onNext: { detail in
+                print("✏️✏️✏️ VC에서 받은 detail---", detail)
+            })
+            .disposed(by: disposeBag)
+    }
+}
