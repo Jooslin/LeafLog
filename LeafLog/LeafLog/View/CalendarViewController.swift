86676424f4aac81de53c8c8e5c7766f5cc2a5fee
devBambu <byrr113@gmail.com>
2026-04-20 19:25:07 +0900

    🩹chore: 샘플 코드 일부 삭제

--- LeafLog/LeafLog/View/CalendarViewController.swift
+++ LeafLog/LeafLog/View/CalendarViewController.swift
@@ -20,7 +20,6 @@ class CalendarViewController: BaseViewController, View {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setSample()
     }
     
     // MARK: - Bind
@@ -87,49 +86,6 @@ extension CalendarViewController {
 //        calendarView.setSnapshot(data)
 //    }
 }
-
-// MARK: - Sample Data Generation
-extension CalendarView.ManageInfoByDate {
-//    static func generateMonthlySampleData() -> [CalendarView.ManageInfoByDate] {
-//        var samples: [CalendarView.ManageInfoByDate] = []
-//        let allBadges: [CalendarView.Badge] = [.grow, .sprout, .water, .treat]
-//        
-//        // 1. 이전 달 데이터 (말일 며칠)
-//        for d in 28...31 {
-//            samples.append(CalendarView.ManageInfoByDate(
-//                currentMonth: false,
-//                day: d,
-//                badge: [] // 빈 Set
-//            ))
-//        }
-//        
-//        // 2. 이번 달 데이터 (1일 ~ 30일)
-//        for d in 1...30 {
-//            // 0~4개 사이의 랜덤한 개수 선택
-//            let randomCount = Int.random(in: 0...4)
-//            // 배지를 무작위로 섞어서 필요한 개수만큼 추출하여 Set으로 생성
-//            let randomBadges = Set(allBadges.shuffled().prefix(randomCount))
-//            
-//            samples.append(CalendarView.ManageInfoByDate(
-//                currentMonth: true,
-//                day: d,
-//                badge: randomBadges
-//            ))
-//        }
-//        
-//        // 3. 다음 달 데이터 (초순 며칠)
-//        for d in 1...5 {
-//            samples.append(CalendarView.ManageInfoByDate(
-//                currentMonth: false,
-//                day: d,
-//                badge: [] // 빈 Set
-//            ))
-//        }
-//        
-//        return samples
-//    }
-}
-
 
 //MARK: CameraClassificationViewController Preview
 @available(iOS 17.0, *)
