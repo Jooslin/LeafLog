a69d30a7a91a132ae7c78a79126b8e84073685fa
yeseul jang <yyssjj9999@gmail.com>
2026-04-21 00:52:55 +0900

    🌟feat: 식물 등록 로직 추가

--- LeafLog/LeafLog/ViewModel/PlantRegisterReactor.swift
+++ LeafLog/LeafLog/ViewModel/PlantRegisterReactor.swift
@@ -10,18 +10,27 @@ import ReactorKit
 import RxSwift
 
 final class PlantRegisterReactor: Reactor {
+    @Dependency(\.plantService) private var plantService
+
     enum Action {
         case viewDidLoad
         case selectPlant(SelectedPlant)
+        case saveTapped(PlantCreateInput)
     }
 
     enum Mutation {
         case setSelectedPlant(SelectedPlant)
+        case setSaving(Bool)
+        case setSaveCompleted
+        case setErrorMessage(String)
     }
 
     struct State {
         var isReady = false
         var selectedPlant: SelectedPlant? = nil
+        var isSaving = false
+        @Pulse var saveCompleted = false
+        @Pulse var errorMessage: String? = nil
     }
 
     let initialState = State()
@@ -32,6 +41,8 @@ final class PlantRegisterReactor: Reactor {
             return .empty()
         case .selectPlant(let selectedPlant):
             return .just(.setSelectedPlant(selectedPlant))
+        case .saveTapped(let input):
+            return savePlant(input: input)
         }
     }
 
@@ -41,8 +52,40 @@ final class PlantRegisterReactor: Reactor {
         switch mutation {
         case .setSelectedPlant(let selectedPlant):
             newState.selectedPlant = selectedPlant
+        case .setSaving(let isSaving):
+            newState.isSaving = isSaving
+        case .setSaveCompleted:
+            newState.isSaving = false
+            newState.saveCompleted = true
+        case .setErrorMessage(let message):
+            newState.isSaving = false
+            newState.errorMessage = message
         }
 
         return newState
+    }
+
+    private func savePlant(input: PlantCreateInput) -> Observable<Mutation> {
+        Observable.create { [plantService] observer in
+            observer.onNext(.setSaving(true))
+
+            let task = Task {
+                do {
+                    _ = try await plantService.registerPlant(input: input)
+                    observer.onNext(.setSaveCompleted)
+                    observer.onCompleted()
+                } catch let error as AuthError {
+                    observer.onNext(.setErrorMessage(error.userMessage))
+                    observer.onCompleted()
+                } catch {
+                    observer.onNext(.setErrorMessage(error.localizedDescription))
+                    observer.onCompleted()
+                }
+            }
+
+            return Disposables.create {
+                task.cancel()
+            }
+        }
     }
 }
