5ff39002aed1ef630151ddcd741ff7adb5d6730d
devBambu <byrr113@gmail.com>
2026-04-23 17:41:05 +0900

    ✨feat: NotificationCenterVC, NotificationCenterView 생성

---
+++ LeafLog/LeafLog/View/NotificationCenterView/NotificationCenterView.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationCenterView.swift
+//  LeafLog
+//
+//  Created by t2025-m0143 on 4/23/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class NotificationCenterView: UIView {
+    
+}
---
+++ LeafLog/LeafLog/View/NotificationCenterView/NotificationCenterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationCenterViewController.swift
+//  LeafLog
+//
+//  Created by t2025-m0143 on 4/23/26.
+//
+
+import UIKit
+import ReactorKit
+
+final class NotificationCenterViewController: BaseViewController, View {
+    
+}
