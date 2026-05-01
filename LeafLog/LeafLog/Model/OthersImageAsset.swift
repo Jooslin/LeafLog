5b79248a02f608b8fcec73d0cd6a5259d2655be3
devBambu <byrr113@gmail.com>
2026-05-01 16:48:02 +0900

    ✨feat: 이미지 에셋 추가

---
+++ LeafLog/LeafLog/Model/OthersImageAsset.swift
@@ -0,0 +1,12 @@
+//
+//  ImageAsset.swift
+//  LeafLog
+//
+//  Created by t2025-m0143 on 5/1/26.
+//
+
+enum OthersImageAsset: String {
+    case cameraColored = "cameraColored"
+    case bellColored = "bellColored"
+    case chatColored = "chatColored"
+}
