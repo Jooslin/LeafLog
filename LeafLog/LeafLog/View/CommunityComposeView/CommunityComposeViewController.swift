9604fc83b318b1b12e7b8f570fad7777748b9b70
Bambu <byrr113@gmail.com>
2026-07-05 02:42:15 +0900

    ✨feat: CommunityComposeVC 생성

---
+++ LeafLog/LeafLog/View/CommunityComposeView/CommunityComposeViewController.swift
@@ -0,0 +1,52 @@
+//
+//  CommunityComposeViewController.swift
+//  LeafLog
+//
+//  Created by 변예린 on 7/5/26.
+//
+
+import UIKit
+import Dependencies
+import ReactorKit
+import RxCocoa
+
+final class CommunityComposeViewController: BaseViewController, View {
+    //MARK: properties
+    let composeView = CommunityComposeView(mode: .create)
+    
+    //MARK: Lifecycle
+    override func loadView() {
+        view = composeView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.reactor = CameraClassificationReactor()
+        tabBarController?.tabBar.isHidden = true
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    //MARK: Bind
+    func bind(reactor: CameraClassificationReactor) {
+        bindAction(reactor: reactor)
+        bindState(reactor: reactor)
+    }
+    
+    private func bindAction(reactor: CameraClassificationReactor) {
+        // 뒤로가기
+        composeView.titleView.rx.backButtonTap
+            .subscribe(onNext: { [weak self] _ in
+                self?.steps.accept(AppStep.pageBack)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindState(reactor: CameraClassificationReactor) {
+        
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+  CommunityComposeViewController()
+}
